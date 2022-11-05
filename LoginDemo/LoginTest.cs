50ddc1668db10b0edbd2a1426fbb02ee32066de3
Charlito <cruizfreak@hotmail.com>
2022-11-04 23:12:37 -0600

    Add SetUp and TearDown hooks

--- LoginDemo/LoginTest.cs
+++ LoginDemo/LoginTest.cs
@@ -6,16 +6,28 @@ namespace LoginDemo
 {
     public class Tests
     {
+        IWebDriver driver = new ChromeDriver(@"C:\driver\chromedriver");
+
+        [SetUp]
+        public void ConfigBrowser()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
+
+        }
 
         [Test]
         public void EnterLoginTrue()
         {
-            IWebDriver driver = new ChromeDriver(@"C:\driver\chromedriver");
-            driver.Manage().Window.Maximize();
-            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
             driver.FindElement(By.Id("username")).SendKeys("tomsmith");
             driver.FindElement(By.Id("password")).SendKeys("SuperSecretPassword!");
             driver.FindElement(By.CssSelector("#login > button")).Click();
+        }
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+            driver.Quit();
         }
     }
 }
